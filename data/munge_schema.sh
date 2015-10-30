7d04cd28890906e5305b6c614b1766d5dd8570ff
Sigfried Gold <sigfried@sigfried.org>
2015-10-30 06:14:59 -0400

    got new data from dqcdm team

---
+++ data/munge_schema.sh
@@ -0,0 +1,12 @@
+psql -U postgres -d postgres -c "drop table if exists $1.dimension_set;"
+psql -U postgres -d postgres -c "drop table if exists $1.measure;"
+psql -U postgres -d postgres -c "drop table if exists $1.result;"
+
+psql -U postgres -d postgres < ./sqldata/$1.sql
+
+psql -U postgres -d postgres -c "drop table if exists $1.denorm;"
+psql -U postgres -d postgres -c "drop table if exists $1.dimensions_regular;"
+psql -U postgres -d postgres -c "drop table if exists $1.results_regular;"
+psql -U postgres -d postgres -c "drop table if exists $1.denorm;"
+
+node dqcdm_munge.js $1
